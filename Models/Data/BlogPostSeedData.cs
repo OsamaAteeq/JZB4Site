873bcd953c0e139834869dc2caf8dd4b54e8871c
OsamaAteeq <r.osamaateeq@gmail.com>
2026-09-02 04:06:10 +0200

    Finish latest updates section

--- Models/Data/BlogPostSeedData.cs
+++ Models/Data/BlogPostSeedData.cs
@@ -8,17 +8,21 @@ public static class BlogPostSeedData
             {
                 Title = "Esports World Cup 2024 Schedule Revealed",
                 Slug = "esports-world-cup-2024-schedule-revealed-gamings-grand-event-in-saudi-arabia",
-                ExcerptHtml = "The official schedule for the Esports World Cup 2024 has been unveiled, setting the stage for an epic showdown in Riyadh with a record-breaking $60 million prize pool.",
-                FeaturedImageUrl = "/images/blog-esports-world-cup.jpg",
-                PublishedDate = new DateTime(2024, 5, 1)
+                Excerpt = "The official schedule for the Esports World Cup 2024 has been unveiled, setting the stage for an epic showdown in Riyadh with a record-breaking $60 million prize pool.",
+                ContentHtml = "The official schedule for the Esports World Cup 2024 has been unveiled, setting the stage for an epic showdown in Riyadh with a record-breaking $60 million prize pool.",
+                FeaturedImageUrl = "/images/esport.jpg",
+                PublishedDate = new DateTime(2024, 5, 1),
+                ImageAltText = "Esport image"
             },
             new BlogPost
             {
                 Title = "Unity and Mazda Collaborate to Revolutionize In-Vehicle Experiences",
                 Slug = "unity-and-mazda-collaborate-to-revolutionize-in-vehicle-experiences",
-                ExcerptHtml = "Unity Japan and Mazda Motor Corporation have announced a groundbreaking partnership aimed at transforming in-vehicle Human-Machine Interfaces.",
-                FeaturedImageUrl = "/images/blog-unity-mazda.jpg",
-                PublishedDate = new DateTime(2024, 5, 1)
+                Excerpt = "Unity Japan and Mazda Motor Corporation have announced a groundbreaking partnership aimed at transforming in-vehicle Human-Machine Interfaces.",
+                ContentHtml = "Unity Japan and Mazda Motor Corporation have announced a groundbreaking partnership aimed at transforming in-vehicle Human-Machine Interfaces.",
+                FeaturedImageUrl = "/images/mazda.jpg",
+                PublishedDate = new DateTime(2024, 5, 1),
+                ImageAltText = "Mazda Interior"
             }
     };
 
